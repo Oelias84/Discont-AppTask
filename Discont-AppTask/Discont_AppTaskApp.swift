1cd3321e55e2100ecca26e31dc33c45bcbc60270
Oelias84 <o.elias84@gmail.com>
2026-07-15 17:20:01 +0300

    Initial Commit

---
+++ Discont-AppTask/Discont_AppTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discont_AppTaskApp.swift
+//  Discont-AppTask
+//
+//  Created by Ofir Elias on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Discont_AppTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
